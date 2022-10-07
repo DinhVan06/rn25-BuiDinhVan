323d00b8f37d3905b7b74f817bff49a3a4346fe4
BuiDinhVan <dinhvanvtpt123@gmail.com>
2022-10-07 13:20:03 +0700

    HomeWord-Responsive-10/5/2022

--- rn25-buidinhvan/src/App.tsx
+++ rn25-buidinhvan/src/App.tsx
@@ -1,53 +1,50 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import imgHouse from './image/1.png';
+import imgHouse from "./image/1.jpg";
 
 function App() {
-  const MAX_NUMBER = 100;
-  let nameCar = { name: "value" };
-  let age_preson = 18;
-  let styles = {
-    color: "red",
-    fontSize: 18,
-  };
   return (
     <div>
       <header>
         <div className="header">
-          <h1>
-            The Pulpit Rock
-          </h1>
+          <h1>The Pulpit Rock</h1>
         </div>
       </header>
       <section>
         <nav>
-          <ul>
-            <li>The Drive</li>
-            <li>The Walk</li>
-            <li>The Return</li>
-            <li>The End</li>
+          <ul className="rows">
+            <li className="item"><a href="#">The Drive</a></li>
+            <li className="item"><a href="#">The Walk</a></li>
+            <li className="item"><a href="#">The Return</a></li>
+            <li className="item"><a href="#">The End</a></li>
           </ul>
         </nav>
         <article>
-          <h1>The Walk</h1>
-          <p>
-            The walk to the Pulpit Rock will take you approximately two hours,
-            give or take an hour depending on the weather conditions and youe
-            physical shape
-          </p>
-          <img src={imgHouse} alt="" />
+          <div className="contain">
+            <h1>The Walk</h1>
+            <p>
+              The walk to the Pulpit Rock will take you approximately two hours,
+              give or take an hour depending on the weather conditions and youe
+              physical shape
+            </p>
+            <div className="contain__image">
+              <img src={imgHouse} alt="" />
+            </div>
+          </div>
         </article>
         <aside>
-          <h1>What?</h1>
-          <p>The Pulpit Rock is a part of a moutain that looks like a pulpit</p>
-          <h1>Where?</h1>
-          <p>The Pulpit Rock is in Norway</p>
-          <h1>Price?</h1>
-          <p>The walk is free!</p>
+          <div className="aside">
+            <h1>What?</h1>
+            <p>The Pulpit Rock is a part of a moutain that looks like a pulpit</p>
+            <h1>Where?</h1>
+            <p>The Pulpit Rock is in Norway</p>
+            <h1>Price?</h1>
+            <p>The walk is free!</p>
+          </div>
         </aside>
       </section>
-      <footer></footer>
+      <footer>Resize the browser window to see how the content respond to the resizing.</footer>
     </div>
   );
 }
